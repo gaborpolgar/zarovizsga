6d2b5f3b81c7fd501c8085b01b98b774339caee5
Gábor Polgár <56192725+gaborpolgar@users.noreply.github.com>
2021-03-16 20:16:46 +0100

    finished the third task.

--- src/main/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHours.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHours.java
@@ -1,4 +1,37 @@
 package hu.nive.ujratervezes.zarovizsga.workhours;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class WorkHours {
+
+
+    public String minWork(String file) {
+        Path readFile = Path.of(file);
+        try (BufferedReader reader = Files.newBufferedReader(readFile)) {
+            return getMinWorkNameWithDate(reader);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Cannot read the file!", ioe);
+        }
+    }
+
+    private String getMinWorkNameWithDate(BufferedReader reader) throws IOException {
+        String name = "";
+        String date = "";
+        String line;
+        long actualWorkhours;
+        long minWorkhours = Long.MAX_VALUE;
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split(",");
+            actualWorkhours = Integer.parseInt(parts[1]);
+            if (actualWorkhours < minWorkhours) {
+                minWorkhours = actualWorkhours;
+                name = parts[0];
+                date = parts[2];
+            }
+        }
+        return name + ": " + date;
+    }
 }
